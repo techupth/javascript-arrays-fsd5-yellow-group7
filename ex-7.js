66c6da6d24dd5b5ef0c903b684e6a4acd3f01d80
datfoi <jengjiening@gmail.com>
2023-11-28 12:22:44 +0700

    feat: assignment-1-7/fai

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,17 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let totalPurchaseOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+console.log(isArray);
+
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+console.log(creditCardTypeOfBlindermann);
+
+let totalPurchaseOfJoannet = orders[3].productQuantity;
+console.log(totalPurchaseOfJoannet);
+
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+console.log(totalPurchaseOfDary);
+
+orders.shift();
+console.log(orders);
